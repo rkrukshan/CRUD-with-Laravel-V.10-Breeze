e0b23b6b3685342d2ef5604b8f7aeccc73fbe5a5
RK Rukshan <44175424+RkRukshan@users.noreply.github.com>
2023-11-14 16:17:40 +0530

    protected $fillable=['name','email','phone'];  in student model

--- .history/app/Models/Student_20231114161618.php
+++ .history/app/Models/Student_20231114161618.php
@@ -8,7 +8,4 @@ use Illuminate\Database\Eloquent\Model;
 class Student extends Model
 {
     use HasFactory;
-
-
-    protected $fillable=['name','email','phone'];
 }
--- .history/app/Models/Student_20231114161629.php
+++ .history/app/Models/Student_20231114161629.php
@@ -10,5 +10,5 @@ class Student extends Model
     use HasFactory;
 
 
-    protected $fillable=['name','email','phone'];
+    protected 
 }
